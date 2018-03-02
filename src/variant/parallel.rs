d1090be19ea2c2cf47664abac5705cb90c045958
Mark LeMoine <thatsgobbles@gmail.com>
2018-03-02 12:40:07 -0800

    Removing all extra ununsed code and comments for now, exploring reference-ification

--- src/variant/parallel.rs
+++ src/variant/parallel.rs
@@ -2,34 +2,10 @@ use std::collections::{HashMap, HashSet, BTreeSet};
 
 use failure::Error;
 
-use variant::gate::Gate;
+use variant::gate::{Slot, Gate};
 use token::{Token, TokenSeq};
 
-pub type UniqueId = u32;
-
-pub type Nodule = UniqueId;
-pub type EdgeId = UniqueId;
-
-// /// Cheffu uses an edge-first system design, where edges represent directed connections between nodules.
-// /// Edges contain most of the interesting information of the graph, including variant gates and tokens.
-// /// It is possible to have multiple edges between a pair of nodules, due to alts and variants.
-// pub struct Edge {
-//     id: EdgeId,
-//     src_nodule: Nodule,
-//     dst_nodule: Nodule,
-//     token_seq: TokenSeq,
-//     gate_op: Option<GateOp>,
-//     // dst_gate_op: Option<GateOp>,
-// }
-
-// /// Set of edge ids outbound for a (implied) nodule.
-// pub type OutEdgeIdSet = HashSet<EdgeId>;
-
-// /// Maps nodules to the ids of edges travelling out from that nodule.
-// pub type NoduleOutEdgeMap = HashMap<Nodule, OutEdgeIdSet>;
-
-// /// Maps edge ids to their edge definitions.
-// pub type EdgeLookupMap = HashMap<EdgeId, Edge>;
+pub type Nodule = u32;
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug, PartialOrd, Ord)]
 pub enum ProcedureItem {
@@ -98,14 +74,14 @@ impl ProcedureGraph {
 #[derive(Debug, Fail, PartialEq, Eq)]
 pub enum GateStackError {
     #[fail(display = "stack is empty")]
-    EmptyStack,
+    Empty,
     #[fail(display = "top of stack does not match; expected: {}, produced: {}", expected, produced)]
-    StackMismatch {
+    Mismatch {
         expected: Gate,
         produced: Gate,
     },
     #[fail(display = "leftover items in stack; found: {:?}", leftover)]
-    StackLeftover {
+    Leftover {
         leftover: Vec<Gate>,
     },
 }
@@ -136,163 +112,20 @@ impl<'a> WalkItemSeq<'a> {
                     gate_stack.push(gate);
                 },
                 &WalkItem::Pop(gate) => {
-                    let popped: &Gate = gate_stack.pop().ok_or(GateStackError::EmptyStack)?;
+                    let popped: &Gate = gate_stack.pop().ok_or(GateStackError::Empty)?;
 
                     // We expect that the top of the stack should match our expected close gate.
-                    ensure!(gate == popped, GateStackError::StackMismatch{expected: gate.clone(), produced: popped.clone()});
+                    ensure!(gate == popped, GateStackError::Mismatch{expected: gate.clone(), produced: popped.clone()});
                 },
             }
         }
 
         // LEARN: `.cloned()` calls `.clone()` on each element of an iterator.
-        ensure!(gate_stack.is_empty(), GateStackError::StackLeftover{leftover: gate_stack.into_iter().cloned().collect()});
+        ensure!(gate_stack.is_empty(), GateStackError::Leftover{leftover: gate_stack.into_iter().cloned().collect()});
 
         Ok(tokens)
     }
 }
-
-// pub struct EdgeIdGen(EdgeId);
-
-// impl EdgeIdGen {
-//     pub fn advance(&mut self) -> EdgeId {
-//         let to_return = self.0.clone();
-//         self.0 += 1;
-//         to_return
-//     }
-// }
-
-// pub struct NoduleGen(Nodule);
-
-// impl NoduleGen {
-//     pub fn advance(&mut self) -> Nodule {
-//         let to_return = self.0.clone();
-//         self.0 += 1;
-//         to_return
-//     }
-// }
-
-// /// Contains the edges, tokens, and gates that comprise all the variants of a single recipe.
-// pub struct ProcedureGraph {
-//     nodule_out_edge_map: NoduleOutEdgeMap,
-//     edge_lookup_map: EdgeLookupMap,
-//     edge_id_gen: EdgeIdGen,
-//     nodule_gen: NoduleGen,
-// }
-
-// impl ProcedureGraph {
-//     /// Creates a new `ProcedureGraph`.
-//     pub fn new() -> Self {
-//         ProcedureGraph {
-//             nodule_out_edge_map: NoduleOutEdgeMap::new(),
-//             edge_lookup_map: EdgeLookupMap::new(),
-//             edge_id_gen: EdgeIdGen(0),
-//             nodule_gen: NoduleGen(0),
-//         }
-//     }
-
-//     /// Connects two nodules together with an edge.
-//     /// This edge will contain information about the tokens present on it, as well as the stack commands on start and close.
-//     pub fn connect(
-//         &mut self,
-//         src_nodule: Nodule,
-//         dst_nodule: Nodule,
-//         token_seq: TokenSeq,
-//         gate_op: Option<GateOp>,
-//         // dst_gate_op: Option<GateOp>,
-//     )
-//     {
-//         // Create a new edge id,
-//         let new_edge_id = self.edge_id_gen.advance();
-
-//         // A new edge needs to be created.
-//         let edge = Edge{
-//             id: new_edge_id,
-//             src_nodule,
-//             dst_nodule,
-//             token_seq,
-//             gate_op,
-//             // dst_gate_op,
-//         };
-
-//         // Add edge id to nodule out edge map, creating if not already existing.
-//         self.nodule_out_edge_map.entry(src_nodule).or_default().insert(new_edge_id);
-
-//         // Add edge and edge id to edge lookup map.
-//         self.edge_lookup_map.insert(new_edge_id, edge);
-//     }
-
-//     pub fn process_procedure_item_seq(
-//         &mut self,
-//         procedure_item_seq: &ProcedureItemSeq,
-//         src_nodule: Nodule,
-//         dst_nodule: Nodule,
-//         gate: Gate,
-//     )
-//     {
-//         // Keep track of the most recent src nodule.
-//         let curr_src_nodule = src_nodule.clone();
-
-//         // Collect tokens encountered directly on this procedure path.
-//         let mut encountered_tokens: TokenSeq = vec![];
-
-//         // LEARN: In this case `procedure_item_seq` is a reference, so `procedure_item` is as well.
-//         for procedure_item in procedure_item_seq {
-//             match procedure_item {
-//                 &ProcedureItem::Token(ref token) => {
-//                     encountered_tokens.push(token.clone());
-//                 },
-//                 &ProcedureItem::Split(ref alt_choices) => {
-//                     // Create new src and dst nodules for the to-be-processed alt choices.
-//                     let alt_src_nodule = self.nodule_gen.advance();
-//                     let alt_dst_nodule = self.nodule_gen.advance();
-
-//                     // Capture current list of encountered tokens.
-//                     // Close off the current path by connecting to the new src nodule.
-//                     self.connect(
-//                         curr_src_nodule,
-//                         alt_src_nodule,
-//                         encountered_tokens,
-//                         gate_op.clone(),
-//                         // dst_gate_op.clone(),
-//                     );
-
-//                     // # We only want to put the stack command on the first out path of a branch, not on any further down.
-//                     // if start_slot_filter_stack_command is not None:
-//                     //     start_slot_filter_stack_command = None
-
-//                     // Reset encountered tokens.
-//                     encountered_tokens = vec![];
-//                 },
-//             };
-//         }
-//     }
-
-//     pub fn process_alt_choice_set(
-//         &mut self,
-//         alt_choice_set: &AltChoiceSet,
-//         src_nodule: Nodule,
-//         dst_nodule: Nodule,
-
-//     )
-//     {
-//         // Normalize the alt choices.
-//         // Each of the resulting alt choices will be 'sandwiched' between the provided src and dst nodules.
-//         let alt_choice_set = normalize_alt_choices(alt_choice_set);
-
-//         for alt_choice in alt_choice_set {
-//             let gate_op = Some(GateOp::Push(alt_choice.active_gate.clone()));
-//             // let dst_gate_op = Some(GateOp::Pop(alt_choice.active_gate.clone()));
-
-//             self.process_procedure_item_seq(
-//                 &alt_choice.proc_items,
-//                 src_nodule,
-//                 dst_nodule,
-//                 gate_op,
-//                 // dst_gate_op,
-//             );
-//         }
-//     }
-// }
 
 #[cfg(test)]
 mod tests {
